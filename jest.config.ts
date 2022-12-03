fdcd2ad2cf5cde03aceb19b66fa1f1c787ecd6b6
Andre Lamparelli <a.lamparelli95@gmail.com>
2022-12-03 13:46:50 +0100

    Project setup

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "@jest/types";
+// Sync object
+const jestConfig: Config.InitialOptions = {
+  verbose: true,
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "^@gb/(.*)$": "<rootDir>/src/$1",
+  },
+  clearMocks: true,
+  resetMocks: true,
+  modulePathIgnorePatterns: ["<rootDir>/dist/"],
+  coveragePathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/test/", "<rootDir>/lib/", "<rootDir>/dist/"]
+};
+export default jestConfig;
